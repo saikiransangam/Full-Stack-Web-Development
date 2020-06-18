dfd0b486f9370a4f0786571d8c554e8b97f60675
Sai Kiran Sangam <51703580+saikiransangam@users.noreply.github.com>
2020-06-18 23:02:51 +0530

    Updated comments

--- Movie Trailer Website/media.py
+++ Movie Trailer Website/media.py
@@ -3,7 +3,7 @@ class Movie():
 	""" Representation of a movie, designed be used with netflix_shows.py """
 
 	def __init__(self, movie_name, movie_poster_image_url, movie_trailer_url):
-		""" Contructor with the arguments required to use fresh_tomatoes.py """
+		""" Contructor with the arguments required to use netflix_shows.py """
 		# Ensure instance variable names are consistent with netflix_shows.py
 		self.title = movie_name
 		self.poster_image_url = movie_poster_image_url
