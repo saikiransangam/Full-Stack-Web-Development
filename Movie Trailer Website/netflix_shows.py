82935beba9cd4d4a65a68b7e148de28698a2d9c7
Sai Kiran Sangam <51703580+saikiransangam@users.noreply.github.com>
2020-06-18 23:07:47 +0530

    Netflix TV shows Trailers

--- Movie Trailer Website/netflix_shows.py
+++ Movie Trailer Website/netflix_shows.py
@@ -75,7 +75,7 @@ main_page_head = '''
               'frameborder': 0
             }));
         });
-        // Animate in the movies when the page loads
+
         $(document).ready(function () {
           $('.movie-tile').hide().first().show("fast", function showNext() {
             $(this).next("div").show("fast", showNext);
